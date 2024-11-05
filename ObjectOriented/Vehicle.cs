69d84401a0312757016497ce8f906cb6543b9293
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2024-11-05 15:19:02 -0700

    Added abstract vehicle class to project

---
+++ ObjectOriented/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingOneReferences.ObjectOriented
+{
+    abstract internal class Vehicle
+    {
+        protected float _speed;
+
+        abstract public void MoveVehicle();
+
+
+    }
+}
